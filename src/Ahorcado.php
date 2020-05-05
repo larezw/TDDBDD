bed2078763cf78e0b8c0c63b1d6dfe3d1ab1cce2
wilfredo <willarez@gmail.com>
2020-05-05 11:38:11 -0300

    Se modifica clase ahorcado y su test

--- src/Ahorcado.php
+++ src/Ahorcado.php
@@ -3,5 +3,52 @@
 namespace GH;
 
 class Ahorcado {
-    
+	private $palabra;
+	private $letrasJugadas = [];
+	private $vidas = 5;
+	
+    public function mostrar()
+    {
+    	$res = '';
+    	for ($i=0; $i < strlen($this->palabra); $i++) {    		
+    		if (in_array($this->palabra[$i], $this->letrasJugadas)) {
+    			$res .= $this->palabra[$i];	
+    		} else {
+    			$res .= '_';
+    		}    		
+    	}
+    	return $res;
+    }
+
+    public function setPalabra(string $palabra) 
+    {
+    	$this->palabra = $palabra;
+    }
+
+    public function jugar(string $letra)
+    {
+    	if ($this->vidas > 0 && !in_array($letra, $this->letrasJugadas)) {
+    		array_push($this->letrasJugadas, $letra);
+	    	if (!$this->verificarJugada($letra)) {
+	    		$this->restarVidas();
+	    	}
+    	}    	
+    }
+
+    private function verificarJugada(string $letra) {
+    	return in_array($letra, str_split($this->palabra));    	
+    }
+
+    private function restarVidas() {
+    	$this->vidas--;
+    }
+
+    public function getVidas()
+    {
+    	return $this->vidas;
+    }
+
+    public function perdio() {
+    	return ($this->vidas == 0);
+    }
 }
--- tests/AhorcadoTest.php
+++ tests/AhorcadoTest.php
@@ -4,8 +4,53 @@ namespace Tests;
 
 final class AhorcadoTest extends \PHPUnit\Framework\TestCase {
 
+    protected function setUp(): void
+    {        
+	   $this->ahorcado = new \GH\Ahorcado;
+    }
+
     public function test01ClaseExiste() {
         $this->assertTrue(class_exists('\GH\Ahorcado'));
     }
+    
+	public function test02Mostrar() {
+		$this->ahorcado->setPalabra('HOLA');
+        $this->assertEquals($this->ahorcado->mostrar(), '____');	
+    }
 
+    public function test03JugarExitoso() {
+		$this->ahorcado->setPalabra('HOLA');
+		$this->ahorcado->jugar('H');
+        $this->assertEquals($this->ahorcado->mostrar(), 'H___');	
+    }
+
+    public function test04JugarErroneo() {
+    	$this->ahorcado->setPalabra('HOLA');
+		$this->ahorcado->jugar('F');
+        $this->assertEquals($this->ahorcado->mostrar(), '____');
+        $this->assertEquals($this->ahorcado->getVidas(), 4);
+    }
+
+    public function test05VerificarQuePierda() {
+    	$this->ahorcado->setPalabra('HOLA');
+		$this->ahorcado->jugar('F');
+		$this->ahorcado->jugar('I');
+		$this->ahorcado->jugar('M');
+		$this->ahorcado->jugar('W'); 
+		$this->ahorcado->jugar('G');       
+        $this->assertTrue($this->ahorcado->perdio());
+    }
+
+     public function test05VerificarQueGane() {
+     	$palabra = 'HOLA';
+    	$this->ahorcado->setPalabra($palabra);
+		$this->ahorcado->jugar('F');
+		$this->ahorcado->jugar('H');
+		$this->ahorcado->jugar('Y');
+		$this->ahorcado->jugar('O');
+		$this->ahorcado->jugar('L'); 
+		$this->ahorcado->jugar('A');       
+        $this->assertEquals($this->ahorcado->mostrar(), $palabra);
+        $this->assertEquals($this->ahorcado->getVidas(), 3);
+    }
 }
