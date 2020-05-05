54849c606b8bac6c7501ef55a28ee07d77f528c0
wilfredo <willarez@gmail.com>
2020-05-05 17:53:30 -0300

    realizando ajustes en clase tateti y su test

--- src/Tateti.php
+++ src/Tateti.php
@@ -5,59 +5,64 @@ namespace GH;
 class Tateti {
 	private $tablero = [];
 	private $simbolo = '';
-	const SIMBOLOS_PERMITIDOS = ['X', 'O'];	
+	const JUGADOR = [
+		'X' => 'player1',
+		'O' => 'player2'
+	];
+	const SIMBOLOS_PERMITIDOS = ['X', 'O'];
 
-    public function crearTablero() : void
-    {
+    public function crearTablero() : void {
     	for ($i=0; $i < 3; $i++) { 
     		for ($j=0; $j < 3; $j++) { 
     			$this->tablero[$i][$j] = '_';
-    		}    		
-    	}    	    	
+    		}
+    	}
     }
 
- 	public function mostrarTablero() 
- 	{
+ 	public function mostrarTablero() {
  		return $this->tablero;
  	}
 
- 	public function jugar(int $x, int $y, string $simbolo) : void
- 	{
+ 	public function jugar(int $x, int $y, string $simbolo) : void {
  		if (!$this->celdaOcupada($x,$y) && in_array($simbolo, self::SIMBOLOS_PERMITIDOS) && $simbolo!=$this->simbolo) {
- 			$this->tablero[$x][$y] = $simbolo;	
+ 			$this->tablero[$x][$y] = $simbolo;
  			$this->simbolo = $simbolo;
  		}
- 		
  	}
 
- 	public function getPosicion(int $x,int $y) 
- 	{
- 		return $this->tablero[$x][$y];
+ 	public function contarFilas() {
+ 		return count($this->tablero);
  	}
 
- 	private function celdaOcupada(int $x, int $y)
- 	{
- 		return ($this->getPosicion($x,$y) != '_');
+ 	public function contarColumnas() {
+ 		return (isset($this->tablero[0])) ? count($this->tablero[0]) : 0;
  	}
 
- 	public function ganoEnFila($fila) 
- 	{
- 		return ($this->tablero[$fila][0] == $this->tablero[$fila][1] && $this->tablero[$fila][1] == $this->tablero[$fila][2]);
+ 	private function celdaOcupada(int $x, int $y) {
+ 		return ($this->tablero[$x][$y] != '_');
  	}
 
- 	public function ganoEnColumna($columna) 
- 	{
- 		return ($this->tablero[0][$columna] == $this->tablero[1][$columna] && $this->tablero[1][$columna] == $this->tablero[2][$columna]);
+ 	public function ganoEnFila($fila) {
+ 		if (in_array($this->tablero[$fila][0],self::SIMBOLOS_PERMITIDOS) && $this->tablero[$fila][0] == $this->tablero[$fila][1] && $this->tablero[$fila][1] == $this->tablero[$fila][2]) {
+			return self::JUGADOR[$this->tablero[$fila][0]];
+		}
+		return false;
  	}
 
- 	public function ganoEnDiagonal() 
- 	{ 		
- 		if ($this->tablero[0][0] == $this->tablero[1][1] && $this->tablero[1][1] == $this->tablero[2][2]) {
- 			return true;
+ 	public function ganoEnColumna($columna) {
+ 		if (in_array($this->tablero[0][$columna],self::SIMBOLOS_PERMITIDOS) && $this->tablero[0][$columna] == $this->tablero[1][$columna] && $this->tablero[1][$columna] == $this->tablero[2][$columna]) {
+ 			return self::JUGADOR[$this->tablero[0][$columna]];
+ 		}
+ 		return false;
+ 	}
+
+ 	public function ganoEnDiagonal() {
+ 		if (in_array($this->tablero[0][0],self::SIMBOLOS_PERMITIDOS) &&  $this->tablero[0][0] == $this->tablero[1][1] && $this->tablero[1][1] == $this->tablero[2][2]) {
+ 			return self::JUGADOR[$this->tablero[0][0]];
  		}
 
- 		if ($this->tablero[0][2] == $this->tablero[1][1] && $this->tablero[1][1] == $this->tablero[2][0]) {
- 			return true;
+ 		if (in_array($this->tablero[0][2],self::SIMBOLOS_PERMITIDOS) && $this->tablero[0][2] == $this->tablero[1][1] && $this->tablero[1][1] == $this->tablero[2][0]) {
+ 			return self::JUGADOR[$this->tablero[0][2]];
  		}
 
  		return false;
--- tests/TatetiTest.php
+++ tests/TatetiTest.php
@@ -4,8 +4,7 @@ namespace Tests;
 
 final class TatetiTest extends \PHPUnit\Framework\TestCase {
 
-	protected function setUp(): void
-    {        
+	protected function setUp(): void {
 	   $this->tateti = new \GH\Tateti;
     }
 
@@ -15,101 +14,87 @@ final class TatetiTest extends \PHPUnit\Framework\TestCase {
 
 	public function test02CrearTablero() {
 		$this->tateti->crearTablero();
-		$tablero= $this->tateti->mostrarTablero();
-        $this->assertEquals(count($tablero), 3);
-        $this->assertEquals(count($tablero[0]), 3);
+        $this->assertEquals($this->tateti->contarFilas(), 3);
+        $this->assertEquals($this->tateti->contarColumnas(), 3);
     }
 
     public function test03Jugar() {
-		$this->tateti->crearTablero();		
-		$this->tateti->jugar(0,0,'X');	
-		$this->tateti->jugar(2,0,'O');		
-		$this->assertEquals($this->tateti->getPosicion(0,0), 'X');
-		$this->assertEquals($this->tateti->getPosicion(0,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(0,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,0), 'O');
-		$this->assertEquals($this->tateti->getPosicion(2,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,2), '_');        
-    } 
+		$this->tateti->crearTablero();
+		$this->tateti->jugar(0,0,'X');
+		$this->tateti->jugar(2,0,'O');
+		$tableroEsperado = [
+			0 => ['X','_','_'],
+			1 => ['_','_','_'],
+			2 => ['O','_','_']
+		];
+		$this->assertEquals($this->tateti->mostrarTablero(), $tableroEsperado);
+    }
 
 
     public function test04JugarSimboloInvalido() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,0,'Z');	
-        $this->assertEquals($this->tateti->getPosicion(0,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(0,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(0,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,2), '_'); 
-    }  
+		$this->tateti->jugar(0,0,'Z');
+		$tableroEsperado = [
+			0 => ['_','_','_'],
+			1 => ['_','_','_'],
+			2 => ['_','_','_']
+		];
+        $this->assertEquals($this->tateti->mostrarTablero(), $tableroEsperado);
+    }
 
     public function test05JugarSimboloRepetido() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,0,'X');	
-		$this->tateti->jugar(1,0,'X');	
-        $this->assertEquals($this->tateti->getPosicion(0,0), 'X');
-		$this->assertEquals($this->tateti->getPosicion(0,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(0,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,2), '_'); 
-    }  
+		$this->tateti->jugar(0,0,'X');
+		$this->tateti->jugar(1,0,'X');
+		$tableroEsperado = [
+			0 => ['X','_','_'],
+			1 => ['_','_','_'],
+			2 => ['_','_','_']
+		];
+       	$this->assertEquals($this->tateti->mostrarTablero(), $tableroEsperado);
+    }
 
     public function test06JugarPosicionRepetida() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,0,'X');	
-		$this->tateti->jugar(0,0,'X');	
-        $this->assertEquals($this->tateti->getPosicion(0,0), 'X');
-		$this->assertEquals($this->tateti->getPosicion(0,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(0,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(1,2), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,0), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,1), '_');
-		$this->assertEquals($this->tateti->getPosicion(2,2), '_'); 
-    } 
+		$this->tateti->jugar(0,0,'X');
+		$this->tateti->jugar(0,0,'X');
+		$tableroEsperado = [
+			0 => ['X','_','_'],
+			1 => ['_','_','_'],
+			2 => ['_','_','_']
+		];	
+		$this->assertEquals($this->tateti->mostrarTablero(), $tableroEsperado);
+    }
 
     public function test07VerificarQueGanaEnFila() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,0,'X');	
+		$this->tateti->jugar(0,0,'X');
 		$this->tateti->jugar(2,0,'O');
-		$this->tateti->jugar(0,1,'X');	
-		$this->tateti->jugar(1,0,'O');	
-		$this->tateti->jugar(0,2,'X');	
-		$this->assertFalse($this->tateti->ganoEnColumna(0), 'X');
-        $this->assertTrue($this->tateti->ganoEnFila(0), 'X');		
+		$this->tateti->jugar(0,1,'X');
+		$this->tateti->jugar(1,0,'O');
+		$this->tateti->jugar(0,2,'X');
+		$this->assertFalse($this->tateti->ganoEnColumna(0));
+        $this->assertEquals($this->tateti->ganoEnFila(0), 'player1');
     }  
 
     public function test08VerificarQueGanaEnColumna() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,1,'X');	
+		$this->tateti->jugar(0,1,'X');
 		$this->tateti->jugar(0,0,'O');
-		$this->tateti->jugar(1,1,'X');	
-		$this->tateti->jugar(1,0,'O');	
-		$this->tateti->jugar(0,2,'X');	
-		$this->tateti->jugar(2,0,'O');	
-        $this->assertTrue($this->tateti->ganoEnColumna(0), 'X');		
-    } 
+		$this->tateti->jugar(1,1,'X');
+		$this->tateti->jugar(1,0,'O');
+		$this->tateti->jugar(0,2,'X');
+		$this->tateti->jugar(2,0,'O');
+        $this->assertEquals($this->tateti->ganoEnColumna(0), 'player2');
+    }
 
     public function test08VerificarQueGanaEnDiagonal() {
 		$this->tateti->crearTablero();
-		$this->tateti->jugar(0,0,'X');	
+		$this->tateti->jugar(0,0,'X');
 		$this->tateti->jugar(1,0,'O');
-		$this->tateti->jugar(1,1,'X');	
-		$this->tateti->jugar(0,2,'O');	
-		$this->tateti->jugar(2,2,'X');			
-        $this->assertTrue($this->tateti->ganoEnDiagonal(0), 'X');		
-    } 
-
+		$this->tateti->jugar(1,1,'X');
+		$this->tateti->jugar(0,2,'O');
+		$this->tateti->jugar(2,2,'X');
+        $this->assertEquals($this->tateti->ganoEnDiagonal(0), 'player1');
+    }
 }
